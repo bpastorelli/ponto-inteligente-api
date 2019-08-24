b3991638d470d29f82543df4e0eafb8759d9ee30
Bruno Pastorelli <brunopastorelli@hotmail.com>
2019-08-24 20:35:00 -0300

    Adicionada dependencia do JPA

---
+++ src/main/java/com/kazale/pontointeligente/api/entities/Empresa.java
@@ -0,0 +1,10 @@
+package com.kazale.pontointeligente.api.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa {
+
+}
